bcf63e4a538146df3b8e4d343c9e30c963dd2a2a
HokageRed <realgreyck@gmail.com>
2019-05-25 23:31:48 +0300

    Flaby

--- js/common.js
+++ js/common.js
@@ -6,18 +6,14 @@ $(document).ready(function(){
       {
         breakpoint: 768,
         settings: {
-          arrows: true,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
+          arrows: false,
+          slidesToShow: 1
         }
       },
       {
         breakpoint: 480,
         settings: {
           arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
           slidesToShow: 1
         }
       }
@@ -25,5 +21,25 @@ $(document).ready(function(){
   });
   $('.slick-next').html('<span style="margin: 0 15px;font-size:18px">Next</span><i class="fas fa-arrow-right"></i>');
   $('.slick-prev').html('<i class="fas fa-arrow-left"></i><span style="margin: 0 15px;font-size:18px">Previous</span>');
-  $('.rel').prepend('<div class="obl" style="z-index: 6;opacity: 0.60;background: #fff;position: absolute;margin: 7% 11%;height: 73%;width: 77%;border: #7c7c7c 1px solid;border-radius: 20px;box-shadow: 0 0 30px rgba(0,0,0,0.5);"></div><div class="obl2" style="z-index: 2;opacity: 0.40;background: #fff;position: absolute;margin: 8% 9%;height: 68%;width: 81%;border: #7c7c7c 1px solid;border-radius: 20px;box-shadow: 0 0 30px rgba(0,0,0,0.5);"></div>');
+  $('.idea-mob-slide').slick({
+    slidesToShow:3,
+    arrows: false,
+      responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 536,
+      settings: {
+        arrows: false,
+        slidesToShow: 1
+      }
+    }
+  ]
+  });
 });
+
